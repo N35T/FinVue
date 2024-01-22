ce98c8d7fb43b2ee97f5ae3157af288ad2e5ca2e
Alex <alex@hagl.de>
2024-01-22 17:28:57 +0100

    Added DB to DI

---
+++ FinVue/FinVue.Data/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FinVue.Data; 
+
+public static class DependencyInjection {
+
+    public static IServiceCollection AddDataServices(this IServiceCollection services, IConfiguration configuration) {
+
+        services.AddDbContext<ApplicationDbContext>(opt => {
+            opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
