fd7aeebb7b1eec9eda29e4414c94d53056b8a189
jas20202 <99250573+jas20202@users.noreply.github.com>
2024-02-04 20:02:40 +0100

    added new Dto

---
+++ FinVue/FinVue.Core/DataTransferObjects/TransactionsByCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿
+using FinVue.Core.Entities;
+
+namespace FinVue.Core.DataTransferObjects;
+public class TransactionsByCategoryDto {
+    public required string CategoryName { get; set; }
+    public required List<Transaction> Transactions { get; set; }
+    public required int TotalSum { get; set; }
+    public required Color CategoryColor { get; set; }
+}
+
