cf0b70c9ff9c5c56aa405e29226ded07366aaf31
Alex <alex@hagl.de>
2024-02-04 17:13:59 +0100

    Added CategoryService UnitTests

---
+++ FinVue/FinVue.Test/TestDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FinVue.Core.Entities;
+using FinVue.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinVue.Test; 
+
+public class TestDbContext : DbContext, IApplicationDbContext {
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<RecurringTransaction> RecurringTransactions { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<User> Users { get; set;  }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseInMemoryDatabase("TestDb");
+    }
+
+    public Task<int> SaveChangesAsync() {
+        return base.SaveChangesAsync();
+    }
+}
