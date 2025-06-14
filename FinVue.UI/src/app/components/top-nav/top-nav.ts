ae58705922830776c2af93682a4f27d69a2e6a7b
jas20202 <99250573+jas20202@users.noreply.github.com>
2025-06-14 13:55:12 +0200

    adds dropdown to pick year

--- FinVue.UI/src/app/components/top-nav/top-nav.ts
+++ FinVue.UI/src/app/components/top-nav/top-nav.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnInit, signal } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTooltip } from '@angular/material/tooltip';
 import { AddCategoryDialog } from '../dialogs/add-category-dialog/add-category-dialog';
 import { BaseDialog } from '../dialogs/base-dialog/base-dialog';
 import { TransactionDialog } from '../dialogs/transaction-dialog/transaction-dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { StatisticService } from '../../services/statistics.service';
+import { BehaviorSubject } from 'rxjs';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { CurrentDateService } from '../../services/current-date-service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-top-nav',
-  imports: [MatTooltip],
+  imports: [MatTooltip, MatFormFieldModule, ReactiveFormsModule , MatSelectModule, AsyncPipe, CommonModule],
   templateUrl: './top-nav.html',
   styleUrl: './top-nav.scss'
 })
-export class TopNav {
+export class TopNav implements OnInit {
 
-    constructor(private matDialog : MatDialog) {}
+    public yearsDropDown = new FormControl();
+
+    public years = new BehaviorSubject<number[]>([]);
+
+    constructor(private matDialog : MatDialog, private statisticService : StatisticService, private router: Router, public currentDateService: CurrentDateService) {}
+
+    ngOnInit(): void {
+        this.statisticService.getYearOfOldestTransaction().subscribe((oldestYear: number) => {
+            let currentYear = new Date().getFullYear();
+            const years = [];
+            while(currentYear >= (oldestYear ?? 2000)) {
+                years.push(currentYear);
+                currentYear--;
+            }
+            this.years.next(years);
+            this.yearsDropDown.setValue(+this.currentDateService.currentDate().year)
+        }); 
+    }
+
+    public navigateToYear(year: number) {
+        this.router.navigateByUrl("/year/" + year);
+    }
 
     public addTransaction() {
         const dialogRef = this.matDialog.open(BaseDialog, {
